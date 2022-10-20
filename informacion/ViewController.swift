decb84f32e7aeb342f3105a3421570e02aae14ec
José Trejo <joseatrejos>
2022-10-20 08:33:44 -0700

    informacion

--- informacion/ViewController.swift
+++ informacion/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblTexto: UILabel!
+    @IBOutlet weak var swPermisoNavegacion: UISwitch!
+    @IBOutlet weak var lblAdvertenciaSwitch: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lblAdvertenciaSwitch.isHidden = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destino = segue.destination as! ModificarController
+        destino.texto = lblTexto.text
+        destino.callBackActualizar = actualizarTexto
     }
 
-
+    @IBAction func doTaoIrOtroVC(_ sender: Any) {
+        lblAdvertenciaSwitch.isHidden = true
+        if swPermisoNavegacion.isOn{
+            performSegue(withIdentifier: "goToModificar", sender: self)
+        }else{
+            lblAdvertenciaSwitch.isHidden = false
+        }
+        
+    }
+    
+    func actualizarTexto(texto: String){
+        lblTexto.text = texto
+    }
+    
+    
 }
 
