d2cf67ccf6653ce6321425517f20719dfe329dac
José Trejo <joseatrejos>
2022-10-20 07:13:29 -0700

    Initial Commit

---
+++ informacion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  informacion
+//
+//  Created by Alumno on 10/20/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
